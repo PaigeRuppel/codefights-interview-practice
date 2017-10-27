88eda944d365749e80dd32378ed713dbd862771f
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-27 13:25:06 -0400

    refactored - tests passing

--- src/main/java/com/paigeruppel/interviewpractice/arrays/Rotation.java
+++ src/main/java/com/paigeruppel/interviewpractice/arrays/Rotation.java
@@ -11,29 +11,14 @@ public class Rotation {
 	 */
 	public int[][] rotateImage(int[][] a) {
 		int[][] rotated = new int[a.length][a.length];
-
-		for (int row = 0; row < a.length; row++) {
+		for (int row = 0, invRow = a.length - 1; row < a.length; row++, invRow--) {
 			for (int col = 0; col < a[0].length; col++) {
-				int newRow = row;
-				int newCol = col;
-				if (col == 0) {
-					newRow = 0;
-				}
-				if (col == 1) {
-					newRow = 1;
-				}
-				if (col == 2) {
-					newRow = a.length - 1;
-				}
-				if (row == 0) {
-					newCol = a.length - 1;
-				}
-				if (row == 1) {
-					newCol = 1;
-				}
-				if (row == 2) {
-					newCol = 0;
-				}
+				//newRow == col
+				int newRow = col;
+				
+				//newCol == reverse of row(count from max...)
+				int newCol = invRow;
+
 				rotated[newRow][newCol] = a[row][col];
 		
 			}
