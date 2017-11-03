cc86bc6b7fe478eeef5a5c21a1fce7395ded6293
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-03 08:29:15 -0400

    passed test to detect conflicts in smaller grids

--- src/main/java/com/paigeruppel/interviewpractice/arrays/SudokuSolve.java
+++ src/main/java/com/paigeruppel/interviewpractice/arrays/SudokuSolve.java
@@ -6,13 +6,12 @@ import java.util.Set;
 public class SudokuSolve {
 
 	public boolean sudoku2(String[][] grid) {
-		return isValidHorizontalAndVertical(grid);
+		return isValidHorizontalAndVertical(grid) && isValidSmallerGrids(grid);
 	}
-	
+
 	private boolean isValidHorizontalAndVertical(String[][] grid) {
 		Set<String> horPresent = new HashSet<>();
 		Set<String> verPresent = new HashSet<>();
-		
 		for (int coord1 = 0; coord1 < grid.length; coord1++) {
 			for (int coord2 = 0; coord2 < grid.length; coord2++) {
 				String horClue = grid[coord1][coord2];
@@ -29,6 +28,30 @@ public class SudokuSolve {
 			}
 			horPresent.clear();
 			verPresent.clear();
+		}
+		return true;
+	}
+
+	private boolean isValidSmallerGrids(String[][] grid) {
+		Set<String> present = new HashSet<>();
+		int colStart = 0;
+		while (colStart < 7) {
+			for (int row = 0; row < grid.length; row++) {
+				for (int col = colStart; col < colStart + 3; col++) {
+					if (row == 3 || row == 6) {
+						present.clear();
+					}
+					String clue = grid[row][col];
+					if (present.contains(clue)) {
+						return false;
+					}
+					if (!clue.equals(".") && !present.contains(clue)) {
+						present.add(clue);
+					}
+				}
+			}
+			present.clear();
+			colStart += 3;
 		}
 		return true;
 	}
