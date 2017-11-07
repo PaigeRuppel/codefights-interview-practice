9611c28ab1e4caa08dc1e088fd9344c8dd722875
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-07 10:03:00 -0500

    some refactoring, using a map to store values instead of two lists

--- src/main/java/com/paigeruppel/interviewpractice/hashtables/RecipeBook.java
+++ src/main/java/com/paigeruppel/interviewpractice/hashtables/RecipeBook.java
@@ -3,8 +3,10 @@ package com.paigeruppel.interviewpractice.hashtables;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class RecipeBook {
@@ -13,7 +15,7 @@ public class RecipeBook {
 		Set<String> uniqueIngredients = new HashSet<>();
 		List<String> repeatIngredients = new ArrayList<>();
 		List<String> temp = new ArrayList<>();
-		
+
 		for (int dish = 0; dish < dishes.length; dish++) {
 			for (int ingredient = 1; ingredient < dishes[dish].length; ingredient++) {
 				String currentIngredient = dishes[dish][ingredient];
@@ -28,34 +30,35 @@ public class RecipeBook {
 		Collections.sort(repeatIngredients);
 		Collections.sort(temp);
 
+		Map<String, String> ingredientsMap = new HashMap<>();
 		for (int dish = 0; dish < dishes.length; dish++) {
 			for (int ingredient = 1; ingredient < dishes[dish].length; ingredient++) {
 				String currentDish = dishes[dish][0];
 				String currentIngredient = dishes[dish][ingredient];
 				if (repeatIngredients.contains(currentIngredient)) {
-					int index = repeatIngredients.indexOf(currentIngredient);
-					String currentEntry = temp.get(index);
-					String newEntry = currentEntry + "," + currentDish;
-					temp.remove(index);
-					temp.add(index, newEntry);
+					if (ingredientsMap.containsKey(currentIngredient)) {
+						String currentValue = ingredientsMap.get(currentIngredient);
+						String newValue = currentValue + "," + currentDish;
+						ingredientsMap.put(currentIngredient, newValue);
+					} else {
+						ingredientsMap.put(currentIngredient, currentDish);
+					}
 				}
 			}
 		}
 
 		String[][] sorted = new String[repeatIngredients.size()][];
-		for (int i = 0 ; i < temp.size(); i ++) {
-			String[] entry = temp.get(i).split(",");
-			String[] recipes = new String[entry.length - 1];
-			for (int k = 0; k < recipes.length; k++) {
-				recipes[k] = entry[k + 1];
+		for (int i = 0; i < repeatIngredients.size(); i++) {
+			String value = ingredientsMap.get(repeatIngredients.get(i));
+			String[] entry = value.split(",");
+			Arrays.sort(entry);
+			String[] complete = new String[entry.length + 1];
+			complete[0] = repeatIngredients.get(i);
+			for (int j = 1; j < complete.length; j++) {
+				complete[j] = entry[j - 1];
 			}
-			Arrays.sort(recipes);
-			for (int j = 1; j < entry.length; j++) {
-				entry[j] = recipes[j - 1];
-			}
-			sorted[i] = entry;
+			sorted[i] = complete;
 		}
-		
 		return sorted;
 	}
 
