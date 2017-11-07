d55022c10ac54383c87c0f25978f8cf6dc2b1107
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-07 09:50:17 -0500

    hacked out a way to sort the array, both tests passing

--- src/main/java/com/paigeruppel/interviewpractice/hashtables/RecipeBook.java
+++ src/main/java/com/paigeruppel/interviewpractice/hashtables/RecipeBook.java
@@ -1,6 +1,7 @@
 package com.paigeruppel.interviewpractice.hashtables;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -44,6 +45,14 @@ public class RecipeBook {
 		String[][] sorted = new String[repeatIngredients.size()][];
 		for (int i = 0 ; i < temp.size(); i ++) {
 			String[] entry = temp.get(i).split(",");
+			String[] recipes = new String[entry.length - 1];
+			for (int k = 0; k < recipes.length; k++) {
+				recipes[k] = entry[k + 1];
+			}
+			Arrays.sort(recipes);
+			for (int j = 1; j < entry.length; j++) {
+				entry[j] = recipes[j - 1];
+			}
 			sorted[i] = entry;
 		}
 		
