d061882c148b0c50336be275408886c9c859c5b9
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-14 09:16:35 -0500

    refactored to use map, eliminated nested for loop

--- src/main/java/com/paigeruppel/interviewpractice/hashtables/IndexComparison.java
+++ src/main/java/com/paigeruppel/interviewpractice/hashtables/IndexComparison.java
@@ -1,18 +1,22 @@
 package com.paigeruppel.interviewpractice.hashtables;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IndexComparison {
 
 	public boolean containsCloseNums(int[] nums, int k) {
-		for (int i = 0; i < nums.length - 1; i++) {
-			int first = nums[i];
-			for (int j = i+1; j < nums.length; j++) {
-				int next = nums[j];
-				if (first == next) {
-					return Math.abs(j - i) <= k;
+		Map<Integer, Integer> indexValue = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int num = nums[i];
+			if (indexValue.containsKey(num)) {
+				int position = indexValue.get(num);
+				if (Math.abs(i - position) <= k) {
+					return true;
 				}
 			}
+			indexValue.put(num, i);
 		}
-		
 		return false;
 	}
 
