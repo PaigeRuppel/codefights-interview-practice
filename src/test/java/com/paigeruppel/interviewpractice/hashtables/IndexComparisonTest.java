71f1fe14dfdd564a9ceef1a685510f60cca17e66
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-14 09:00:03 -0500

    added two additional test cases, all passing, solution passed, before refactoring

--- src/test/java/com/paigeruppel/interviewpractice/hashtables/IndexComparisonTest.java
+++ src/test/java/com/paigeruppel/interviewpractice/hashtables/IndexComparisonTest.java
@@ -8,23 +8,38 @@ import org.junit.Test;
 public class IndexComparisonTest {
 
 	private IndexComparison underTest;
-	
-	@Before 
+
+	@Before
 	public void setup() {
 		underTest = new IndexComparison();
 	}
-	
+
 	@Test
 	public void shouldReturnTrueTestCase1() {
-		int[] nums = { 0, 1, 2, 3, 5, 2};
+		int[] nums = { 0, 1, 2, 3, 5, 2 };
 		int k = 3;
 		assertTrue(underTest.containsCloseNums(nums, k));
 	}
-	
+
 	@Test
 	public void shouldReturnFalseTestCase2() {
-		int[] nums = { 0, 1, 2, 3, 5, 2};
+		int[] nums = { 0, 1, 2, 3, 5, 2 };
 		int k = 2;
 		assertFalse(underTest.containsCloseNums(nums, k));
 	}
+
+	@Test
+	public void shouldReturnFalseTestCase3() {
+		int[] nums = {};
+		int k = 0;
+		assertFalse(underTest.containsCloseNums(nums, k));
+	}
+
+	@Test
+	public void shouldReturnTrueTestCase4() {
+		int[] nums = { 99, 99 };
+		int k = 2;
+		assertTrue(underTest.containsCloseNums(nums, k));
+	}
+
 }
