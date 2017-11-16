c3941feeeb201a71fe3f781a65b53a37238a1c51
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-16 16:17:28 -0500

    tried additional looping - tests not yet passing - before trying to build a second array of all coins present based off of quantities

--- src/main/java/com/paigeruppel/interviewpractice/hashtables/CoinOrganizer.java
+++ src/main/java/com/paigeruppel/interviewpractice/hashtables/CoinOrganizer.java
@@ -1,37 +1,44 @@
 package com.paigeruppel.interviewpractice.hashtables;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class CoinOrganizer {
 
 	public int possibleSums(int[] coins, int[] quantities) {
-		Map<Integer, Integer> coinMap = new HashMap<>();
 		Set<Integer> sums = new HashSet<>();
 		List<Integer> rawSums = new ArrayList<>();
 		for (int i = 0; i < coins.length; i++) {
 			int coin = coins[i];
 			int quantity = quantities[i];
-			coinMap.put(coin, quantity);
 			int q = 1;
 			do {
 				sums.add(coin * q);
 				rawSums.add(coin * q);
 				q++;
-			} while (q < quantity);
+			} while (q <= quantity);
 		}
-
-		for (int i = 0; i < rawSums.size() - 1; i++) {
-			int temp = rawSums.get(i) + rawSums.get(i + 1);
-			sums.add(temp);
+		
+		for (int i = 0; i < coins.length; i++) {
+			for (int j = i + 1; j < coins.length + 1; j++) {
+				int first = rawSums.get(i);
+				int next = rawSums.get(j);
+				sums.add(first + next);
+				rawSums.add(first + next);
+			}
+		}
+		
+		for (int i = 0; i < rawSums.size() - 1; i ++) {
+			for (int j = i + 1; j < rawSums.size(); j++) {
+				int first = rawSums.get(i);
+				int next = rawSums.get(j);
+				sums.add(first + next);
+			}
 		}
 
 		return sums.size();
 	}
-
 
 }
