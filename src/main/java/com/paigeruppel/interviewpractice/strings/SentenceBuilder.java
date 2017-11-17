130ce13fdf00da3fee04cde690652e79d59d72e8
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-17 12:59:22 -0500

    eliminated loop by using join method on String

--- src/main/java/com/paigeruppel/interviewpractice/strings/SentenceBuilder.java
+++ src/main/java/com/paigeruppel/interviewpractice/strings/SentenceBuilder.java
@@ -3,16 +3,9 @@ package com.paigeruppel.interviewpractice.strings;
 public class SentenceBuilder {
 
 	public String amendTheSentence(String s) {
-		StringBuilder sb = new StringBuilder();
 		String[] words = s.split("(?=[A-Z])");
-		for (int i = 0; i < words.length; i++) {
-			String word = words[i].toLowerCase();
-			sb.append(word);
-			if (i != words.length - 1) {
-				sb.append(" ");
-			}
-		}
-		return sb.toString();
+		StringBuilder ammended = new StringBuilder(String.join(" ", words));
+		return ammended.toString().toLowerCase();
 	}
 
 }
