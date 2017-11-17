a8d67c17f730341cb8ce291255735111ecbe8ad5
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-17 09:16:22 -0500

    refactored into a while loop with a variable increment, not yet passing

--- src/main/java/com/paigeruppel/interviewpractice/hashtables/CoinOrganizer.java
+++ src/main/java/com/paigeruppel/interviewpractice/hashtables/CoinOrganizer.java
@@ -21,43 +21,49 @@ public class CoinOrganizer {
 				q++;
 			} while (q <= quantity);
 		}
-		
-		for (int i = 0; i < availableCoins.size() - 1; i++) {
-			int coin1 = availableCoins.get(i);
-			int temp = coin1;
-			for (int j = i + 1; j < availableCoins.size(); j++) {
-				int coin2 = availableCoins.get(j);
-				temp += coin2; 
-				sums.add(coin1 + coin2);
-				sums.add(temp);
-			}
-			sums.add(temp);
-		}
 
-		for (int i = 0; i < availableCoins.size() - 1; i++) {
-			int coin1 = availableCoins.get(i);
-			int temp = coin1;
-			for (int j = i + 2; j < availableCoins.size(); j++) {
-				int coin2 = availableCoins.get(j);
-				temp += coin2; 
-				sums.add(coin1 + coin2);
-				sums.add(temp);
-			}
-			sums.add(temp);
-		}
+		int increment = availableCoins.size() - 1;
 
-		for (int i = 0; i < availableCoins.size() - 1; i++) {
-			int coin1 = availableCoins.get(i);
-			int temp = coin1;
-			for (int j = i + 3; j < availableCoins.size(); j++) {
-				int coin2 = availableCoins.get(j);
-				temp += coin2; 
-				sums.add(coin1 + coin2);
+		while (increment > 0) {
+
+			for (int i = 0; i < availableCoins.size() - 1; i++) {
+				int coin1 = availableCoins.get(i);
+				int temp = coin1;
+				for (int j = i + increment; j < availableCoins.size() && j > 0; j--) {
+					int coin2 = availableCoins.get(j);
+					temp += coin2;
+					sums.add(coin1 + coin2);
+					sums.add(temp);
+				}
 				sums.add(temp);
 			}
-			sums.add(temp);
+			increment--;
 		}
 		
+		// for (int i = 0; i < availableCoins.size() - 1; i++) {
+		// int coin1 = availableCoins.get(i);
+		// int temp = coin1;
+		// for (int j = i + 2; j < availableCoins.size(); j++) {
+		// int coin2 = availableCoins.get(j);
+		// temp += coin2;
+		// sums.add(coin1 + coin2);
+		// sums.add(temp);
+		// }
+		// sums.add(temp);
+		// }
+		//
+		// for (int i = 0; i < availableCoins.size() - 1; i++) {
+		// int coin1 = availableCoins.get(i);
+		// int temp = coin1;
+		// for (int j = i + 3; j < availableCoins.size(); j++) {
+		// int coin2 = availableCoins.get(j);
+		// temp += coin2;
+		// sums.add(coin1 + coin2);
+		// sums.add(temp);
+		// }
+		// sums.add(temp);
+		// }
+
 		return sums.size();
 	}
 
