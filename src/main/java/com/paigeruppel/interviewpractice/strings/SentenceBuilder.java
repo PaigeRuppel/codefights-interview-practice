bff367e9921e0614e4194667bda89e7e00154a6c
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-17 12:55:28 -0500

    passed three test cases using regex

--- src/main/java/com/paigeruppel/interviewpractice/strings/SentenceBuilder.java
+++ src/main/java/com/paigeruppel/interviewpractice/strings/SentenceBuilder.java
@@ -3,7 +3,16 @@ package com.paigeruppel.interviewpractice.strings;
 public class SentenceBuilder {
 
 	public String amendTheSentence(String s) {
-		return "codefights is awesome";
+		StringBuilder sb = new StringBuilder();
+		String[] words = s.split("(?=[A-Z])");
+		for (int i = 0; i < words.length; i++) {
+			String word = words[i].toLowerCase();
+			sb.append(word);
+			if (i != words.length - 1) {
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
 	}
 
 }
--- src/test/java/com/paigeruppel/interviewpractice/strings/SentenceBuilderTest.java
+++ src/test/java/com/paigeruppel/interviewpractice/strings/SentenceBuilderTest.java
@@ -20,5 +20,17 @@ public class SentenceBuilderTest {
 		String s = "CodefightsIsAwesome";
 		assertThat(underTest.amendTheSentence(s), is("codefights is awesome"));
 	}
+	
+	@Test
+	public void shouldReturnFormattedStringTestCase2() {
+		String s = "Hello";
+		assertThat(underTest.amendTheSentence(s), is("hello"));
+	}
+	
+	@Test
+	public void shouldReturnFormattedStringTestCase3() {
+		String s = "vSKwWDjwIerQKMgVaAniorRJlerbKpDgvyKBLPNwSRWtylqKewNFtERNuUBBHAsGkTSSfdhQHJYvAighAdeG";
+		assertThat(underTest.amendTheSentence(s), is("v s kw w djw ier q k mg va anior r jlerb kp dgvy k b l p nw s r wtylq kew n ft e r nu u b b h as gk t s sfdh q h j yv aigh ade g"));
+	}
 
 }
