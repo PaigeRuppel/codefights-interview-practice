5d168fdc9223875f1ef7a9155dd0d8f7ab2141b8
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-17 13:22:52 -0500

    passed with custom logic, before refactoring

--- src/main/java/com/paigeruppel/interviewpractice/strings/PatternDetection.java
+++ src/main/java/com/paigeruppel/interviewpractice/strings/PatternDetection.java
@@ -3,7 +3,24 @@ package com.paigeruppel.interviewpractice.strings;
 public class PatternDetection {
 
 	public int findFirstSubstringOccurence(String s, String x) {
-		return s.indexOf(x);
+		char[] allChars = s.toCharArray();
+		int sLength = s.length();
+		char[] match = x.toCharArray();
+		int xLength = x.length();
+		for (int i = 0; i <= sLength - xLength; i++) {
+			int count = 0;
+			if (allChars[i] == match[0]) {
+				while (count < xLength) {
+					if (allChars[i + count] == match[count] && count == xLength - 1) {
+						return i;
+					} else if (allChars[i + count] != match[count]) {
+						break;
+					}
+					count++;
+				}
+			}
+		}
+		return -1;
 	}
 
 }
