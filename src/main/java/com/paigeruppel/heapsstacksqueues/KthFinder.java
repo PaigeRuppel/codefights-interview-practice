b355df3c13b0f6a45ea57f5916aec638d0feb26f
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-22 07:23:40 -0500

    wrote logic for method, passing two test cases

--- src/main/java/com/paigeruppel/heapsstacksqueues/KthFinder.java
+++ src/main/java/com/paigeruppel/heapsstacksqueues/KthFinder.java
@@ -3,7 +3,18 @@ package com.paigeruppel.heapsstacksqueues;
 public class KthFinder {
 
 	public int kthLargestElement(int[] nums, int k) {
-		return 6;
+	    for (int i = 0; i < nums.length; i++) {
+	        for (int j = i + 1; j < nums.length; j++) {
+	            int tmp = 0;
+	            if (nums[i] < nums[j]) {
+	                tmp = nums[i];
+	                nums[i] = nums[j];
+	                nums[j] = tmp;
+	            }
+	        }
+	    }
+		
+		return nums[k - 1];
 	}
 
 }
