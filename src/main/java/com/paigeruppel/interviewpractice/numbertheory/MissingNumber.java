8f0f21e49e8b0a928326333a953ffe5dc601a333
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-28 09:11:09 -0500

    refactored to use integer sum equation

--- src/main/java/com/paigeruppel/interviewpractice/numbertheory/MissingNumber.java
+++ src/main/java/com/paigeruppel/interviewpractice/numbertheory/MissingNumber.java
@@ -1,22 +1,14 @@
 package com.paigeruppel.interviewpractice.numbertheory;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class MissingNumber {
 
 	public int findMissingNumber(int[] arr) {
 		int n = arr.length;
-		List<Integer> numbers = Arrays.stream(arr).boxed().collect(Collectors.toList());
-		int count = 0;
-		while (count <= n) {
-			if (!numbers.contains(count)) {
-				return count;
-			}
-			count++;
+		int sum = n * (n+1) / 2;
+		for (int num:arr) {
+			sum -= num;
 		}
-		return 0;
+		return sum;
 	}
 
 }
