a5b3775ae27c4f86b261b723f50b041d0c02b1a4
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-28 09:03:48 -0500

    first three test cases passing

--- src/main/java/com/paigeruppel/interviewpractice/numbertheory/MissingNumber.java
+++ src/main/java/com/paigeruppel/interviewpractice/numbertheory/MissingNumber.java
@@ -1,9 +1,22 @@
 package com.paigeruppel.interviewpractice.numbertheory;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MissingNumber {
 
 	public int findMissingNumber(int[] arr) {
-		return 2;
+		int n = arr.length;
+		List<Integer> numbers = Arrays.stream(arr).boxed().collect(Collectors.toList());
+		int count = 0;
+		while (count <= n) {
+			if (!numbers.contains(count)) {
+				return count;
+			}
+			count++;
+		}
+		return 0;
 	}
 
 }
