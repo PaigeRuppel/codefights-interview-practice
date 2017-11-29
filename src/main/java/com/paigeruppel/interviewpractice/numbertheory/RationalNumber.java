0aa08eedf74afbba86ffa40af74b91e112bfcb93
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-29 10:41:07 -0500

    refactored to find gcd as part of state, passed solution

--- src/main/java/com/paigeruppel/interviewpractice/numbertheory/RationalNumber.java
+++ src/main/java/com/paigeruppel/interviewpractice/numbertheory/RationalNumber.java
@@ -4,32 +4,24 @@ public class RationalNumber {
 
 	private int numerator;
 	private int denominator;
-	private int[] ratio = {numerator, denominator};
+	private int gcd;
+	private int[] ratio = new int[2];
 	
 	public RationalNumber(int numerator, int denominator) {
 		this.numerator = numerator;
 		this.denominator = denominator;
+		gcd = findGreatestCommonDenominator(numerator, denominator);
 	}
 	
 	public int[] simplifyRational() {
-		reduce();
+		ratio[0] = numerator / gcd;
+		ratio[1] = denominator / gcd;
 		flipNegative();
-		checkForZero();
 		return ratio;
 	}
 
-	private void reduce() {
-		int factor = 2;
-		while (factor <= Math.abs(numerator)) {
-			if (numerator % factor == 0 && denominator % factor == 0) {
-				numerator /= factor;
-				denominator /= factor;
-			} else {
-				factor++;
-			}
-		}
-		ratio[0] = numerator;
-		ratio[1] = denominator;
+	private int findGreatestCommonDenominator(int a, int b) {
+		return b == 0 ? a : findGreatestCommonDenominator(b, a % b);
 	}
 
 	private void flipNegative() {
@@ -39,9 +31,4 @@ public class RationalNumber {
 		}
 	}
 
-	private void checkForZero() {
-		if (ratio[0] == 0) {
-			ratio[1] = 1;
-		}
-	}
 }
