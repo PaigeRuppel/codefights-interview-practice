41a88c5d2a3166610f5a9cf188143c8be007ae36
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-29 08:36:15 -0500

    passed solution, after refactoring with numerator and denominator as part of state, before refactoring to implement greatest common denominator logic

--- src/main/java/com/paigeruppel/interviewpractice/numbertheory/RationalNumber.java
+++ src/main/java/com/paigeruppel/interviewpractice/numbertheory/RationalNumber.java
@@ -2,9 +2,46 @@ package com.paigeruppel.interviewpractice.numbertheory;
 
 public class RationalNumber {
 
-	public int[] simplifyRational(int numerator, int denominator) {
-		int[] reduced = { 1, 2 };
-		return reduced;
+	private int numerator;
+	private int denominator;
+	private int[] ratio = {numerator, denominator};
+	
+	public RationalNumber(int numerator, int denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+	
+	public int[] simplifyRational() {
+		reduce();
+		flipNegative();
+		checkForZero();
+		return ratio;
 	}
 
+	private void reduce() {
+		int factor = 2;
+		while (factor <= Math.abs(numerator)) {
+			if (numerator % factor == 0 && denominator % factor == 0) {
+				numerator /= factor;
+				denominator /= factor;
+			} else {
+				factor++;
+			}
+		}
+		ratio[0] = numerator;
+		ratio[1] = denominator;
+	}
+
+	private void flipNegative() {
+		if (ratio[1] < 0) {
+			ratio[0] = -ratio[0];
+			ratio[1] = -ratio[1];
+		}
+	}
+
+	private void checkForZero() {
+		if (ratio[0] == 0) {
+			ratio[1] = 1;
+		}
+	}
 }
--- src/test/java/com/paigeruppel/interviewpractice/numbertheory/RationalNumberTest.java
+++ src/test/java/com/paigeruppel/interviewpractice/numbertheory/RationalNumberTest.java
@@ -10,15 +10,34 @@ public class RationalNumberTest {
 
 	private RationalNumber underTest;
 
-	@Before
-	public void setup() {
-		underTest = new RationalNumber();
+	
+
+	@Test
+	public void simplifyShouldReturnReduced1Over2FromNumerator3AndDenominator6() {
+		underTest = new RationalNumber(3, 6);
+		int[] simplified = { 1, 2 };
+		assertThat(underTest.simplifyRational(), is(simplified));
 	}
 
 	@Test
-	public void simplifyShouldReturnReduced12FromNumerator3AndDenominator6() {
-		int[] simplified = { 1, 2 };
-		assertThat(underTest.simplifyRational(3, 6), is(simplified));
+	public void simplifyShouldReturnUnchanged5Over8FromNumerator5AndDenominator8() {
+		underTest = new RationalNumber(5, 8);
+		int[] simplified = { 5, 8 };
+		assertThat(underTest.simplifyRational(), is(simplified));
 	}
+	
+	@Test
+	public void simplifyShouldReturnFlippedNegative8Over5FromNumerator8AndDenominatorNegative5() {
+		underTest = new RationalNumber(8, -5);
+		int[] simplified = { -8, 5 };
+		assertThat(underTest.simplifyRational(), is(simplified));
+	} 
+	
 
+	@Test
+	public void simplifyShouldReturn0Over1FromNumerator0AndDenominatorNegative239() {
+		underTest = new RationalNumber(0, -239);
+		int[] simplified = { 0, 1 };
+		assertThat(underTest.simplifyRational(), is(simplified));
+	} 
 }
