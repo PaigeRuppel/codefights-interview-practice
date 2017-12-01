74dafb83e29c9ecc001f57e1a3c68f4300de2e1b
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-01 15:35:38 -0500

    implemented logic with fibonacci series recursion

--- src/main/java/com/paigeruppel/interviewpractice/dynamicprogramming/basic/Staircase.java
+++ src/main/java/com/paigeruppel/interviewpractice/dynamicprogramming/basic/Staircase.java
@@ -5,7 +5,14 @@ public class Staircase {
 	// you can take the staircase 1 or 2 steps at a time
 	// return the total possible ways to climb a staircase with n steps
 	public int climbingStairs(int n) {
-		return n;
+		return fibonacci(n) + fibonacci(n - 1);
+	}
+
+	private int fibonacci(int n) {
+		if (n <= 1) {
+			return n;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
 
 }
--- src/test/java/com/paigeruppel/interviewpractice/dynamicprogramming/basic/StaircaseTest.java
+++ src/test/java/com/paigeruppel/interviewpractice/dynamicprogramming/basic/StaircaseTest.java
@@ -24,4 +24,20 @@ public class StaircaseTest {
 	public void climbingStairsWith2StepsShouldReturn2Combinations() {
 		assertThat(underTest.climbingStairs(2), is(2));
 	}
+	
+	@Test
+	public void climbingStairsWith4StepsShouldReturn5Combinations() {
+		assertThat(underTest.climbingStairs(4), is(5));
+	}
+
+	@Test
+	public void climbingStairsWith13StepsShouldReutrn377Combinations() {
+		assertThat(underTest.climbingStairs(13), is(377));
+	}
+	
+	@Test
+	public void climbingStairsWith26StepsShouldReturn196418Combinations() {
+		assertThat(underTest.climbingStairs(26), is(196418));
+	}
+	
 }
