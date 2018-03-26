eb58f7d1e833aaf1e63a2206d8521b02a0cfdab3
Paige Ruppel <paige.ruppel@gmail.com>
2018-03-26 09:30:46 -0400

    started exercise to maximize the sum from non-adjacent elements in an array - passed first test case

---
+++ src/main/java/com/paigeruppel/interviewpractice/dynamicprogramming/basic/NonAdjacentMaximizer.java
@@ -0,0 +1,7 @@
+package com.paigeruppel.interviewpractice.dynamicprogramming.basic;
+
+public class NonAdjacentMaximizer {
+    public int houseRobber(int[] nums) {
+        return 2;
+    }
+}
---
+++ src/test/java/com/paigeruppel/interviewpractice/dynamicprogramming/basic/NonAdjacentMaximizerTest.java
@@ -0,0 +1,24 @@
+package com.paigeruppel.interviewpractice.dynamicprogramming.basic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class NonAdjacentMaximizerTest {
+
+    private NonAdjacentMaximizer underTest;
+
+    @Before
+    public void setup() {
+        underTest = new NonAdjacentMaximizer();
+    }
+
+    @Test
+    public void arrayOfLength3WithAllOnesShouldReturn2() {
+        int[] nums = {1,1,1};
+        assertThat(underTest.houseRobber(nums), is(2));
+    }
+
+}
